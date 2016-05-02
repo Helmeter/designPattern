d57c6b8da73d9e8146fba425e1d303be6b982149
helmeter <leehongjian@126.com>
2016-05-02 13:52:31 +0800

    单例模式

--- src/main/java/singleton/Client.java
+++ src/main/java/singleton/Client.java
@@ -1,8 +1,6 @@
 package singleton;
 
-/**
- * Created by frank on 2015/12/27.
- */
+
 public class Client {
     public static  void main(String[] args){
 
--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -2,9 +2,7 @@ package singleton;
 
 import java.util.Date;
 
-/**
- * Created by frank on 2015/12/27.
- */
+
 public class Singleton {
     private static Singleton uniqueInstance = null;
     private Date date = null ;  // 对象创建的时间
