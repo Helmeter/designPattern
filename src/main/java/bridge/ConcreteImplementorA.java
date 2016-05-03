f83edff57f8823d77191f2162a4180f956922b43
helmeter <leehongjian@126.com>
2016-05-03 19:31:25 +0800

    桥接模式

---
+++ src/main/java/bridge/ConcreteImplementorA.java
@@ -0,0 +1,12 @@
+package bridge;
+
+/**
+ * Created by frank on 2016/2/1.
+ */
+public class ConcreteImplementorA implements  Implementor {
+
+
+    public void operationImpl() {
+        System.out.println("ConcreteImplementorA operationImpl");
+    }
+}
---
+++ src/main/java/bridge/Implementor.java
@@ -0,0 +1,8 @@
+package bridge;
+
+/**
+ * Created by frank on 2016/2/1.
+ */
+public interface Implementor {
+    public void operationImpl();
+}
