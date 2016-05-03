1ab97c9ac9acfbb09d8316a30c5b8dd5a069e27f
helmeter <leehongjian@126.com>
2016-05-03 09:47:42 +0800

    完善单例模式

--- src/main/java/singleton/Client.java
+++ src/main/java/singleton/Client.java
@@ -5,7 +5,7 @@ public class Client {
     public static  void main(String[] args){
 
         for (int i=0;i<10;++i){
-            Singleton sgt = Singleton.getInstance();
+            Singleton2 sgt = Singleton2.getInstance();
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -11,11 +11,14 @@ public class Singleton {
         date = new Date();
     }
     public static synchronized Singleton getInstance(){
+       // synchronized 同步其实降低了性能,只有第一次调用时才需要同步,初始了uniqueInstance后便不需要再进行同步
         if (uniqueInstance == null){
             uniqueInstance = new Singleton();
         }
         return uniqueInstance;
     }
+    //修复性能问题1.声明变量时,初始化2.双重检查加锁 详见singleton2
+
 
     // do something
     public void  doSomething(){
