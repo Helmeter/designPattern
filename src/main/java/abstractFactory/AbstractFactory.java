c0fedf8540ec1b6eceb86b28a538bd25daab9a78
helmeter <leehongjian@126.com>
2016-05-05 11:25:21 +0800

    uodate

--- src/main/java/abstractFactory/AbstractFactory.java
+++ src/main/java/abstractFactory/AbstractFactory.java
@@ -2,7 +2,7 @@ package abstractFactory;
 
 public interface AbstractFactory {
 
-    public AbstractProductA createProductA();
+     AbstractProductA createProductA();
 
-    public AbstractProductB createProductB();
+     AbstractProductB createProductB();
 }
